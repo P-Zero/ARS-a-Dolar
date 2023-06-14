43479f9831b55e8ddd354f2db25bfcd1b9a63b88
Benjamin Quevedo <quevedobenjamin2@gmail.com>
2023-06-13 23:26:12 -0300

    Mobile First CSS and visual improves

--- script.js
+++ script.js
@@ -5,16 +5,16 @@ const PESOS = document.querySelector('#pesos')
 function calculator(pesos){
     if (pesos == 0){
         TYPES.innerHTML = `
-        <li class="usd blue"><span>Blue:</span> $${PRICES[0]}</li>
-        <li class="usd oficial"><span>Oficial:</span> $${PRICES[1]}</li>
-        <li class="usd liqui"><span>Contado con Liqui:</span> $${PRICES[2]}</li>
-        <li class="usd cripto"><span>Cripto:</span> $${PRICES[3]}</li>`
+        <li class="usd blue"><p>Blue: </p><span>$${PRICES[0]}</span></li>
+        <li class="usd oficial"><p>Oficial: </p><span>$${PRICES[1]}</span></li>
+        <li class="usd liqui"><p>Contado con Liqui: </p><span>$${PRICES[2]}</span></li>
+        <li class="usd cripto"><p>Cripto: </p><span>$${PRICES[3]}</span></li>`
     }
     else{
         TYPES.innerHTML = `
-        <li class="usd blue"><span>Blue:</span> $${Intl.NumberFormat('de-DE').format(Math.round(pesos / PRICES[0]))}</li>
-        <li class="usd oficial"><span>Oficial:</span> $${Intl.NumberFormat('de-DE').format(Math.round(pesos / PRICES[1]))}</li>
-        <li class="usd liqui"><span>Contado con Liqui:</span> $${Intl.NumberFormat('de-DE').format(Math.round(pesos / PRICES[2]))}</li>
-        <li class="usd cripto"><span>Cripto:</span> $${Intl.NumberFormat('de-DE').format(Math.round(pesos / PRICES[3]))}</li>`
+        <li class="usd blue"><p>Blue: </p><span>$${Intl.NumberFormat('de-DE').format(Math.round(pesos / PRICES[0]))}</span></li>
+        <li class="usd oficial"><p>Oficial: </p><span>$${Intl.NumberFormat('de-DE').format(Math.round(pesos / PRICES[1]))}</span></li>
+        <li class="usd liqui"><p>Contado con Liqui: </p><span>$${Intl.NumberFormat('de-DE').format(Math.round(pesos / PRICES[2]))}</span></li>
+        <li class="usd cripto"><p>Cripto: </p><span>$${Intl.NumberFormat('de-DE').format(Math.round(pesos / PRICES[3]))}</span></li>`
     }
 }
