f5df39d0e92984a375136570f9947e6791154279
Benjamin Quevedo <quevedobenjamin2@gmail.com>
2023-11-21 13:57:20 -0300

    added 'dolar tarjeta', code optimization and UI improvements

--- script.js
+++ script.js
@@ -1,52 +1,84 @@
-async function calculator(){
-    const res = await fetch('https://api.bluelytics.com.ar/v2/latest')
-    const data = await res.json()
+function calculator() {
     const LIST = document.querySelector('#currency_list')
     const CHANGE_BUTTON = document.querySelector('#change_mode')
-    const INPUT = document.querySelector('#currency')
+    const INPUT = document.querySelector('.currency_value')
+    const PLACEHOLDER = document.querySelector('.currency_type')
+    const INPUT_CONTAINER = document.querySelector('.currency_container')
+    const data = {}
     let boolean = true
+
+    function fetchData() {
+        fetch('https://api.bluelytics.com.ar/v2/latest')
+            .then((res) => res.json())
+            .then((apiData) => {
+                data.blue = apiData.blue
+                data.oficial = apiData.oficial
+                data.blue_euro = apiData.blue_euro
+                data.oficial_euro = apiData.oficial_euro
+                calcs()
+            })
+            .catch((error) => console.error('Error fetching data', error))
+    }
 
     CHANGE_BUTTON.addEventListener('click', () => {
         boolean = !boolean
         calcs()
     })
+
     INPUT.addEventListener('change', calcs)
     INPUT.addEventListener('keyup', calcs)
+    INPUT_CONTAINER.addEventListener('click', () => {
+        INPUT.focus()
+    })
+
+    function getInputValue() {
+        return parseFloat(INPUT.value) || 0
+    }
+
+    function formatNumber(value) {
+        if (value >  100) {
+            return Intl.NumberFormat('de-DE', {
+                minimumFractionDigits: 0,
+                maximumFractionDigits: 0,
+            }).format(value)
+        }
+        return Intl.NumberFormat('de-DE', {
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2,
+        }).format(value)
+    }
+
+    function calcs() {
+        const inputValue = getInputValue()
+        const currencyTypeCrry = boolean ? 'ARS' : 'USD / EUR'
+        const currencyTypeInput = boolean ? 'USD / EUR' : 'ARS'
     
-    function getInputValue(){
-        return document.querySelector('#currency').value
-    }
+        const currencyList = [
+            { label: 'Dólar Blue', value: data.blue.value_sell },
+            { label: 'Dólar Oficial', value: data.oficial.value_sell },
+            { label: 'Dólar Tarjeta', value: data.oficial.value_sell * 2 },
+            { label: 'Euro Blue', value: data.blue_euro.value_sell },
+            { label: 'Euro Oficial', value: data.oficial_euro.value_sell },
+        ]
+    
+        LIST.innerHTML = currencyList
+            .map((currency) => {
+                let convertedValue = currency.value
+                
+                if (inputValue > 0) {
+                    convertedValue = formatNumber(boolean ? inputValue * currency.value : inputValue / currency.value)
+                }
 
-    function calcs(){
-        if(getInputValue() <= 0){
-            LIST.innerHTML = `
-            <li class="usd blue"><p>Dólar Blue: </p><span>$${data.blue.value_sell} <p class="crry">USD</p></span></li>
-            <li class="usd oficial"><p>Dólar Oficial: </p><span>$${data.oficial.value_sell} <p class="crry">USD</p></span></li>
-            <li class="eur blue"><p>Euro Blue: </p><span>$${data.blue_euro.value_sell} <p class="crry">EUR</p></span></li>
-            <li class="eur oficial"><p>Euro Oficial: </p><span>$${data.oficial_euro.value_sell} <p class="crry">EUR</p></span></li>` 
-        } else if(boolean){
-            LIST.innerHTML = `
-            <li class="usd blue"><p>Dólar Blue: </p><span>$${Intl.NumberFormat('de-DE', { minimumFractionDigits: 2, maximumFractionDigits: 2 }).format(getInputValue() / data.blue.value_sell)} <p class="crry">USD</p></span></li>
-            <li class="usd oficial"><p>Dólar Oficial: </p><span>$${Intl.NumberFormat('de-DE', { minimumFractionDigits: 2, maximumFractionDigits: 2 }).format(getInputValue() / data.oficial.value_sell)} <p class="crry">USD</p></span></li>
-            <li class="eur blue"><p>Euro Blue: </p><span>$${Intl.NumberFormat('de-DE', { minimumFractionDigits: 2, maximumFractionDigits: 2 }).format(getInputValue() / data.blue_euro.value_sell)} <p class="crry">EUR</p></span></li>
-            <li class="eur oficial"><p>Euro Oficial: </p><span>$${Intl.NumberFormat('de-DE', { minimumFractionDigits: 2, maximumFractionDigits: 2 }).format(getInputValue() / data.oficial_euro.value_sell)} <p class="crry">EUR</p></span></li>` 
-        }
-        else{
-            LIST.innerHTML = `
-            <li class="usd blue"><p>Dólar Blue: </p><span>$${Intl.NumberFormat('de-DE', { minimumFractionDigits: 2, maximumFractionDigits: 2 }).format(getInputValue() * data.blue.value_sell)} <p class="crry">ARS</p></span></li>
-            <li class="usd oficial"><p>Dólar Oficial: </p><span>$${Intl.NumberFormat('de-DE', { minimumFractionDigits: 2, maximumFractionDigits: 2 }).format(getInputValue() * data.oficial.value_sell)} <p class="crry">ARS</p></span></li>
-            <li class="eur blue"><p>Euro Blue: </p><span>$${Intl.NumberFormat('de-DE', { minimumFractionDigits: 2, maximumFractionDigits: 2 }).format(getInputValue() * data.blue_euro.value_sell)} <p class="crry">ARS</p></span></li>
-            <li class="eur oficial"><p>Euro Oficial: </p><span>$${Intl.NumberFormat('de-DE', { minimumFractionDigits: 2, maximumFractionDigits: 2 }).format(getInputValue() * data.oficial_euro.value_sell)} <p class="crry">ARS</p></span></li>`
-        }
-
-        if(boolean){
-            INPUT.placeholder = "ARS"
-        }
-        else{
-            INPUT.placeholder = "USD / EUR"
-        }
+                return `<li><p>${currency.label}: </p><p>$${convertedValue} <span class="crry">${currencyTypeCrry}</span></p></li>`
+            })
+            .join('')
+    
+        PLACEHOLDER.innerText = currencyTypeInput
     }
-    calcs()
+    
+    fetchData()
 }
+
 calculator()
+
 
